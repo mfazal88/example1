16a00c5bcd0e2ea332ea610cf3f603ca51652a3d
Chamath Wijeweera <chamath@chamaths-mini.home>
2015-03-12 17:33:46 +0530

    Initial Commit

---
+++ StarRating/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StarRating
+//
+//  Created by Chamath Wijeweera on 3/12/15.
+//  Copyright (c) 2015 Chamath Wijeweera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
